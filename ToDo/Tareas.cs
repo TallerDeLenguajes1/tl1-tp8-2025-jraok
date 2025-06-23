898a6a532fbff15391a826bb9615b093e93bc60d
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-23 01:28:49 -0300

    primera version de la clase Tarea

---
+++ ToDo/Tareas.cs
@@ -0,0 +1,27 @@
+using System;
+namespace Tareas;
+
+public class Tarea{
+    // campos del objeto tarea
+    private int tareaId, duracion;
+    private string descripcion;
+
+    // propiedades solo de lectura para el objeto
+    public int TareaId => tareaId;
+    public string Descripcion => descripcion;
+    public int Duracion => duracion;
+
+    // semilla para crear numeros aleatorios
+    private static Random semilla = new Random();
+
+    // constructor
+    public Tarea(int IdIngresado, string texto){
+        if (string.IsNullOrEmpty(texto))
+        {
+            throw new ArgumentException("\nLa descripción no puede estar vacía, REINGRESE");
+        }
+        this.tareaId = IdIngresado;
+        this.duracion = semilla.Next(10,101);
+        this.descripcion = texto;
+    }
+}
